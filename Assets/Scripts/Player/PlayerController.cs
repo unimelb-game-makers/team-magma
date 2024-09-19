e1a52a50f80b73e10826a529206273e49d7620a7
Rin <87895505+lvqq2000@users.noreply.github.com>
2024-09-19 16:49:27 +1000

    Revert "Add the ability to jump"

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,10 +12,6 @@ namespace Player
         [Header("Movement Setup")]
         [SerializeField] private float movementSpeed = 15f;
         [SerializeField] private OrientationType orientation;
-        [SerializeField] private float jumpHeight = 2.0f;
-    	[SerializeField] private float jumpForce = 2.0f;
-    
-    	
 
         [Space(10)]
 
@@ -59,14 +55,10 @@ namespace Player
 
         private bool _isMovingHorizontally;
         private bool _isMovingVertically;
-        private bool _isGrounded;
-
         private float _previousDodge;
         private Vector3 _dodgeDirection;
-
         private float _horizontalInput;
         private float _verticalInput;
-
         private void Start()
         {
             // Get the Rigidbody component attached to this GameObject
@@ -156,12 +148,6 @@ namespace Player
                 _DodgeButtonDown = false;
             }
 
-            if(Input.GetButtonDown("Jump") && _isGrounded){
-    
-    			_rigidbody.AddForce(new Vector3(0.0f, jumpHeight, 0.0f) * jumpForce, ForceMode.Impulse);
-    			_isGrounded = false;
-    		}
-
             if (Time.time > _previousDodge + isInvulnerableTime && GetComponent<Damageable>().getIsInvulnerable()) {
                 GetComponent<Damageable>().setIsInvulnerable(false);
             }
@@ -185,6 +171,9 @@ namespace Player
                 {
                     movement = new Vector3(_horizontalInput * movementSpeed, _rigidbody.velocity.y, _verticalInput * movementSpeed);
                 }
+
+                // Apply the movement to the Rigidbody
+                //_rigidbody.MovePosition(_rigidbody.position + movement * Time.deltaTime);
 
                 // Apply velocity to the Rigidbody
                 _rigidbody.velocity = movement;
@@ -294,10 +283,5 @@ namespace Player
             }
             return _isMovingHorizontally || _isMovingVertically;
         }
-
-        void OnCollisionStay()
-    	{
-    		_isGrounded = true;
-    	}
     }
 }
