9e92e4f8a9cd48934e19eb9b89053defb529da2b
peiyuw2 <peiyuw08@gmail.com>
2024-12-11 19:24:59 +1100

    Remove Easy Save 3

--- Assets/Scripts/Hazard/Hazard.cs
+++ Assets/Scripts/Hazard/Hazard.cs
@@ -19,45 +19,5 @@ namespace Hazard
         {
             throw new System.NotImplementedException();
         }
-
-        public void OnChangeTrack()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnChangeTempo()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnChangeBar()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnChangeBeat()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void EnableSync()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SyncLow()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SyncMid()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void SyncHigh()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
--- Assets/Scripts/Tempo/ISyncable.cs
+++ Assets/Scripts/Tempo/ISyncable.cs
@@ -5,13 +5,5 @@ namespace Tempo
     public interface ISyncable : IGameService
     {
         void Affect(TapeType tapeType, float duration, float effectValue);
-        void OnChangeTrack();
-        void OnChangeTempo();
-        void OnChangeBar();
-        void OnChangeBeat();
-        void EnableSync();
-        void SyncLow();
-        void SyncMid();
-        void SyncHigh();
     }
 }
