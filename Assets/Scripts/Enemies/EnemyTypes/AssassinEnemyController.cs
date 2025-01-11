803322c0b108d15b333d273b36abe1beede674d8
WilliamAlexTW <118658885+WilliamAlexTW@users.noreply.github.com>
2025-01-11 22:13:56 +1100

    Implemented melee enemy damage

--- Assets/Scripts/Enemies/EnemyTypes/AssassinEnemyController.cs
+++ Assets/Scripts/Enemies/EnemyTypes/AssassinEnemyController.cs
@@ -17,7 +17,7 @@ public class AssassinEnemyController : EnemyController
     // Temp variables for collision checking - if all enemy types are using this, should declare in the base class
     [Header("Knockback")]
     [SerializeField] private float knockbackForce = 10f;
-    [SerializeField] private float knockbacDuration = 1f;
+    [SerializeField] private float knockbackDuration = 1f;
     private bool hasCollidedWithPlayer = false;
     public bool isKnockback = false;
     private float currentknockbackTime;
@@ -51,9 +51,6 @@ public class AssassinEnemyController : EnemyController
 
                 // If player is still alive.
                 if (GetPlayerController()) StartDash();
-
-                // TO IMPLEMENT: damageable (Check MeleeEnemyController for more details).
-                // damageable will take care of dealing the damage.
             }
         }     
     }
@@ -80,7 +77,7 @@ public class AssassinEnemyController : EnemyController
 
             if (hasCollidedWithPlayer) {
                 isKnockback = true;
-                currentknockbackTime = knockbacDuration;
+                currentknockbackTime = knockbackDuration;
                 GetAnimator().SetTrigger(knockbackAnimationTrigger);
 
                 // Apply a knockback:
@@ -102,7 +99,7 @@ public class AssassinEnemyController : EnemyController
     // Could implement this for all enemy types and apply a knockback to all of them, interrupting their attack.
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Player")) {
-            Debug.Log("Enemy has collided with the player");
+            Debug.Log("Assassin Enemy has collided with the player");
             hasCollidedWithPlayer = true;
         }
     }
