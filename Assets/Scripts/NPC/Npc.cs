bbed00282dd4391e39bfc86d6601b951fab7c468
peiyuw2 <peiyuw08@gmail.com>
2025-01-28 13:07:03 +1100

    Add Distance Check

--- Assets/Scripts/NPC/Npc.cs
+++ Assets/Scripts/NPC/Npc.cs
@@ -2,8 +2,10 @@
 // 28 01 2025 01 19
 
 using System;
+using System.Collections;
 using Ink.Runtime;
 using Narrative;
+using Player;
 using UnityEngine;
 
 namespace NPC
@@ -16,6 +18,7 @@ namespace NPC
         private TextAsset inkJSONAsset; // Assign the compiled LvlPocLeaderStory.json here
         
         private Story _story;
+        [SerializeField] private float _interactionRange = 5f;
 
         private NpcState _currentState = NpcState.Idle; public NpcState CurrentState => _currentState;
         
@@ -25,14 +28,50 @@ namespace NPC
             _story = new Story(inkJSONAsset.text);
         }
         
+        
+        
+        
+        private GameObject _player = null;
+        private Coroutine _playerInRangeCheckCoroutine = null;
+        
         private void OnMouseDown()
         {
+            if (!IsPlayerInRange()) return;
+            
             if (CurrentState == NpcState.Idle)
             {
                 DialogueUi.Instance.TalkToNpc(_story);
             }
+            
+            _playerInRangeCheckCoroutine = StartCoroutine(PlayerInRangeCheck());
+        }
+
+        private bool IsPlayerInRange()
+        {
+            if(_player == null) _player = GameObject.FindGameObjectWithTag("Player");
+            //check distance 
+            return Vector3.Distance(_player.transform.position, transform.position) < _interactionRange;
         }
         
+        
+        /// <summary>
+        /// Coroutine to check if player is in range
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator PlayerInRangeCheck()
+        {
+            while (true)
+            {
+                if (!IsPlayerInRange())
+                {
+                    StopCoroutine(_playerInRangeCheckCoroutine);
+                    _playerInRangeCheckCoroutine = null;
+                    DialogueUi.Instance.HideUI();
+                    yield break;
+                }
+                yield return new WaitForSeconds(1f);
+            }
+        }
     }
 
     public enum NpcState
