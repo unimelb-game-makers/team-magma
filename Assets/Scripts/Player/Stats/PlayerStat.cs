687a39cfb68b10ea3b15c7f9ac87821264d41c00
peiyuw2 <peiyuw08@gmail.com>
2025-03-10 13:00:52 +1100

    Add health immunity period

---
+++ Assets/Scripts/Player/Stats/PlayerStat.cs
@@ -0,0 +1,124 @@
+// Author : Peiyu Wang @ Daphatus
+// 10 03 2025 03 11
+
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Player.Stats
+{
+    public enum StatType
+    {
+        Health,
+        Battery,
+    }
+    
+    [Serializable]
+    public abstract class PlayerStat : MonoBehaviour
+    {
+        [SerializeField]
+        private float maxValue = 100f;
+        
+        public float MaxValue 
+        { 
+            get => maxValue; 
+            protected set => maxValue = value; 
+        }
+
+        [SerializeField]
+        private float threshold = 0.2f;
+        public float Threshold 
+        { 
+            get => threshold; 
+            protected set => threshold = value; 
+        }
+        
+        [SerializeField]
+        private float currentValue;
+        public float CurrentValue
+        {
+            get => currentValue;
+            protected set
+            {
+                bool oldBelow = currentValue <= threshold;
+                currentValue = Mathf.Clamp(value, 0, maxValue);
+                OnValueChanged?.Invoke(currentValue);
+                bool newBelow = currentValue <= threshold;
+                if (oldBelow != newBelow)
+                {
+                    if(newBelow)
+                    {
+                        OnOnBelowThreshold();
+                    }
+                    else
+                    {
+                        OnOnExceedingThreshold();
+                    }
+                }
+            }
+        }
+        
+        public UnityAction<float> OnValueChanged;
+        public event Action onBelowThreshold;
+        public event Action onAboveThreshold;
+        public PlayerStat( )
+        {
+            currentValue = maxValue; 
+        }
+        
+        public PlayerStat(float maxValue, float currentValue)
+        {
+            this.maxValue = maxValue;
+            this.currentValue = currentValue;
+        }
+
+        public abstract StatType StatType { get; }
+        
+        public abstract float Modify(float amount);
+
+        protected virtual void OnOnBelowThreshold()
+        {
+            onBelowThreshold?.Invoke();
+        }
+
+        protected virtual void OnOnExceedingThreshold()
+        {
+            onAboveThreshold?.Invoke();
+        }
+        
+        public void IncreaseMaxValue(float amount)
+        {
+            MaxValue += amount;
+        }
+        public virtual void DecreaseMaxValue(float amount)
+        {
+            MaxValue -= amount;
+            if (CurrentValue > MaxValue)
+            {
+                CurrentValue = MaxValue;
+            }
+            if (MaxValue <= 0)
+            {
+                MaxValue = 0;
+                CurrentValue = 0;
+            }
+        }
+        /// <summary>
+        /// To initialize components after other systems have been initialized.
+        /// </summary>
+        public virtual void OnEnabled()
+        {
+            
+        }
+        
+        /// <summary>
+        /// To clean up components before the system is destroyed.
+        /// </summary>
+        public virtual void CleanUp()
+        {
+            OnValueChanged = null;
+            onBelowThreshold = null;
+            onAboveThreshold = null;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,12 @@
+// Author : Peiyu Wang @ Daphatus
+// 10 03 2025 03 52
+
+using UnityEngine;
+
+namespace UI
+{
+    public class HealthBarUI : MonoBehaviour
+    {
+        
+    }
+}
