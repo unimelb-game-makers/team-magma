cdae74de6603836ccb2a045f97f720129ee4c320
Rin <lvqq2000@gmail.com>
2025-04-13 16:06:11 +1000

    feat(success screen): add success screen when the player exist room1

---
+++ Assets/Scripts/UI/SuccessScreen/SuccessScreenTriggerTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UI;
+
+// Note that this is temporary for alpha build
+
+[RequireComponent(typeof(Collider))]
+public class SuccessScreenTriggerTest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                // make sure the enemy sfx is stopped
+                SoundManager.Instance.StopAllSFX();
+
+                SuccessScreenManager.Instance.ShowSuccessScreen();
+            }
+        }
+}
