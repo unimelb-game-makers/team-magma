44d8f6a36b39ae9a686197e6313660ca1101cc60
Alex Kam <kamzhanyue@gmail.com>
2025-07-31 23:21:31 +1000

    refactor: change tempo mode to support dictionary

--- Assets/Scripts/Tempo/TempoSetting.cs
+++ Assets/Scripts/Tempo/TempoSetting.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public enum TempoMode
 {
     Slow,
@@ -5,24 +7,44 @@ public enum TempoMode
     Fast
 }
 
+public class GameSetting
+{
+    public float BpmMultiplier { get; set; }
+    public float PlayerMovementMultiplier { get; set; }
+    public float EnemyMovementMultiplier { get; set; }
+}
+
 public static class TempoSetting
 {
     public const int TIME_SIGNATURE = 4;
-    public const float SlowRatio    = 0.875f; // ~140 BPM
-    public const float DefaultRatio = 1.0f;   // 160 BPM
-    public const float FastRatio    = 1.125f; // ~180 BPM
+
+    private static readonly Dictionary<TempoMode, GameSetting> Tempos = new()
+    {
+        // 140 BPM
+        { TempoMode.Slow, new GameSetting { BpmMultiplier = 0.875f, PlayerMovementMultiplier = 1f, EnemyMovementMultiplier = 0.5f } },
+        // 160 BPM
+        { TempoMode.Default, new GameSetting { BpmMultiplier = 1f, PlayerMovementMultiplier = 1f, EnemyMovementMultiplier = 1f } },
+        // 180 BPM
+        { TempoMode.Fast, new GameSetting { BpmMultiplier = 1.125f, PlayerMovementMultiplier = 1.5f, EnemyMovementMultiplier = 1f } },
+    };
+    
 
     /// <summary>
     /// Gets the playback speed ratio for the specified TempoMode.
     /// </summary>
     public static float GetRatio(TempoMode mode)
     {
-        return mode switch
-        {
-            TempoMode.Slow    => SlowRatio,
-            TempoMode.Fast    => FastRatio,
-            _                 => DefaultRatio
-        };
+        return Tempos[mode].BpmMultiplier;
+    }
+
+    public static float GetPlayerMovementMultiplier(TempoMode mode)
+    {
+        return Tempos[mode].PlayerMovementMultiplier;
+    }
+
+    public static float GetEnemyMovementMultiplier(TempoMode mode)
+    {
+        return Tempos[mode].EnemyMovementMultiplier;
     }
 }
 
